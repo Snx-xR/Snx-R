44228a506d2fe383e8c3b3b2caf127ad3a3fb4b4
Sa Sajjad <sqsa93103@gmail.com>
2023-10-07 22:39:03 +0600

    Let's build SiXtY-N9
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -8,24 +8,23 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch nad_lavender-userdebug # lunch command 
 # lunch nad_lavender-eng # build type eng
-# export some module
+# export extra module
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
 export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export RELAX_USES_LIBRARY_CHECK=true
-# vanilla build
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
+# vanilla build
 mka nad
 # gapps build
-export USE_GAPPS=true
-mka nad
+# export USE_GAPPS=true
+# mka nad
 # only specific app build
 # export BOARD_USES_SYSTEMUI=true
 # mka SystemUI
 # bash <(curl -s $script/upload_apps.sh)
-# idk what is this
 rclone copy out/target/product/$device/*.zip cirrus:$device -P
