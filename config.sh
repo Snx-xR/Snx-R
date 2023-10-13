55f0938353e1843dc25baf30d1eecc5a0ee98e99
Sa Sajjad <sqsa93103@gmail.com>
2023-10-13 12:42:29 +0600

    try

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 # add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b battery -g default,-mips,-darwin,-notdefault
 # replace with your manifest
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten .repo/local_manifests
 # sync script
@@ -19,12 +19,12 @@ make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-mka nad
+# mka nad
 # gapps build
 # export USE_GAPPS=true
 # mka nad
 # only specific app build
-# export BOARD_USES_SYSTEMUI=true
-# mka SystemUI
-# bash <(curl -s $script/upload_apps.sh)
+export BOARD_USES_SYSTEMUI=true
+mka SystemUI
+bash <(curl -s $script/upload_apps.sh)
 rclone copy out/target/product/$device/*.zip cirrus:$device -P
