94836aa5ebff208e247246cf0c98360e0f59401f
Sa Sajjad <sqsa93103@gmail.com>
2023-11-07 16:00:15 +0600

    sysui test

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 # add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b testbg -g default,-mips,-darwin,-notdefault
 # replace with your manifest
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten .repo/local_manifests #ten-se-permissive
 # sync script
@@ -16,18 +16,20 @@ export RELAX_USES_LIBRARY_CHECK=true
 export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export RELAX_USES_LIBRARY_CHECK=true
-make api-stubs-docs || echo no problem
-make system-api-stubs-docs || echo no problem
-make test-api-stubs-docs || echo no problem
+
+# make api-stubs-docs || echo no problem
+# make system-api-stubs-docs || echo no problem
+# make test-api-stubs-docs || echo no problem
+
 # vanilla build
-mka nad
+# mka nad
 
 # gapps build
 # export USE_GAPPS=true
 # mka nad
 
 # # only specific app build
-# export BOARD_USES_SYSTEMUI=true
-# mka SystemUI
-# bash <(curl -s $script/upload_apps.sh)
+export BOARD_USES_SYSTEMUI=true
+mka SystemUI
+bash <(curl -s $script/upload_apps.sh)
 rclone copy out/target/product/$device/*.zip cirrus:$device -P
