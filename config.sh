5d24238b7e22ae0d93d5d3486fccd3482f44dc52
Sa Sajjad <sqsa93103@gmail.com>
2023-12-05 21:40:51 +0600

    fuk matrixx 14
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,13 +1,12 @@
 # add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 14.0 -g default,-mips,-darwin,-notdefault
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests #ten-se-permissive
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b matrix-14 .repo/local_manifests
 # sync script
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
-lunch nad_lavender-userdebug # lunch command 
-# lunch nad_lavender-eng # build type/lunch command 
+lunch lineage_lavender-userdebug
 
 # export extra module
 export TZ=Asia/Dhaka
@@ -16,18 +15,10 @@ export RELAX_USES_LIBRARY_CHECK=true
 export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export RELAX_USES_LIBRARY_CHECK=true
+# some extra module
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-mka nad
-
-# gapps build
-# export USE_GAPPS=true
-# mka nad
-
-# # only specific app build
-# export BOARD_USES_SYSTEMUI=true
-# mka SystemUI
-# bash <(curl -s $script/upload_apps.sh)
+m bacon
 Snx-R
