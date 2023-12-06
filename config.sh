ce5c95f81b3134a67af7d2426754de3503cef43a
Sa Sajjad <sqsa93103@gmail.com>
2023-12-06 22:54:56 +0600

    fuk
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # add repo, dt, vt, kt etc.
-# repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 14.0 -g default,-mips,-darwin,-notdefault
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectMatrixx/android.git -b 14.0 -g default,-mips,-darwin,-notdefault
+# repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 # replace with your manifest
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b matrix-14 .repo/local_manifests
 # sync script
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-# repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+# repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
 lunch lineage_lavender-userdebug
@@ -18,8 +18,8 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export RELAX_USES_LIBRARY_CHECK=true
 # Gapps Build
-export WITH_GAPPS=true
-export EXTRA_GAPPS=false
+# export WITH_GAPPS=true
+# export EXTRA_GAPPS=false
 
 # some extra module
 m bacon
