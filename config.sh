9d46cefc045117e3e47ced50cbaba238e654987b
Sa Sajjad <sqsa93103@gmail.com>
2023-12-11 14:52:47 +0600

    Matrix 14 bring up
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 # add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b stock -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --git-lfs --no-repo-verify https://github.com/ProjectMatrixx/android.git -b 14.0 -g default,-mips,-darwin,-notdefault
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests #ten-se-permissive
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b matrix-14 .repo/local_manifests
 # sync script
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
 export BUILD_USERNAME=snaxpsycho
 export BUILD_HOSTNAME=snaxpsycho_beta
-lunch nad_lavender-userdebug 
+lunch lineage_lavender-userdebug
 
 # export extra module
 export TZ=Asia/Dhaka
@@ -21,7 +21,7 @@ make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-mka nad
+m bacon
 
 # gapps build
 # export USE_GAPPS=true
