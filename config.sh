f186ce6c293135b10c95ad30089fbc51e5218403
Sa Sajjad <sqsa93103@gmail.com>
2023-12-17 21:12:39 +0600

    test'
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b cr-11 .repo/local_m
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
-# lunch lineage_lavender-userdebug
+lunch lineage_lavender-userdebug
 # export extra module
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
@@ -14,12 +14,13 @@ export RELAX_USES_LIBRARY_CHECK=true
 export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export RELAX_USES_LIBRARY_CHECK=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-# m bacon
-brunch lineage_lavender-userdebug
+m bacon
+# brunch lineage_lavender-userdebug
 # gapps build
 # export USE_GAPPS=true
 # mka nad
