5fdc23e0a3f077eee7dfa1a9dc3f5cfaa10170a1
Sa Sajjad <sqsa93103@gmail.com>
2023-12-17 16:05:08 +0600

    Bring up cr-11
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b cr-11 .repo/local_m
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
+lunch lineage_lavender-userdebug
 # export extra module
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
@@ -17,7 +18,7 @@ make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-brunch lineage_lavender-userdebug
+m bacon
 
 # gapps build
 # export USE_GAPPS=true
