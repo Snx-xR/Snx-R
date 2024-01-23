d51639f4e3f262426725d686811efffe5758d999
Sa Sajjad <sqsa93103@gmail.com>
2024-01-23 22:16:45 +0600

    test dt
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 # add repo, dt, vt, kt etc.
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests #ten-se-permissive
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-test .repo/local_manifests #ten-se-permissive
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 lunch nad_lavender-userdebug
 # export extra module
 export TZ=Asia/Dhaka
-export SELINUX_IGNORE_NEVERALLOWS=true
+# export SELINUX_IGNORE_NEVERALLOWS=true
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
