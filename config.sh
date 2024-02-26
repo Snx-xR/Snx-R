ba5c8931b3655b842ff508319cad3c5d22cd5488
Sa Sajjad <sqsa93103@gmail.com>
2024-02-26 17:18:58 +0600

    Bring up
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # add repo, dt, vt, kt etc.
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests #ten-se-permissive
+git clone https://github.com/Amritorock/local_manifest --depth 1 -b 10 .repo/local_manifests #ten-se-permissive
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch nad_r5x-userdebug
 # export extra module
 export TZ=Asia/Dhaka
 # export SELINUX_IGNORE_NEVERALLOWS=true
@@ -15,7 +15,7 @@ make test-api-stubs-docs || echo no problem
 # vanilla build
 mka nad
 export GAPPS_BUILD=false
-export RELEASE=true
+export RELEASE=false
 Snx-R
 # export GAPPS_BUILD=true
 # # only specific app build
