3db406fbf6b65cac621e3b09817d29f66fc164c0
Sa Sajjad <sqsa93103@gmail.com>
2024-03-12 12:30:11 +0600

    supext
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 ///# add repo, dt, vt, kt etc.
 # repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/SuperiorExtended/manifest -b UDC --git-lfs
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b try .repo/local_manifests #ten-se-permissive
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b try .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 
-lunch lineage_lavender-userdebug
+lunch superior_lavender-eng
 # export extra module
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
@@ -19,8 +19,8 @@ export RELAX_USES_LIBRARY_CHECK=true
 # make system-api-stubs-docs || echo no problem
 # make test-api-stubs-docs || echo no problem
 # vanilla build
-m bacon
-export GAPPS_BUILD=true
+mka bacon
+export GAPPS_BUILD=no
 export RELEASE=false
 Snx-R
 # export GAPPS_BUILD=true
