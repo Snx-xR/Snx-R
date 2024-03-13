5ebd33331c568cb8ef9406b33eaa5ef4888291c7
Sa Sajjad <sqsa93103@gmail.com>
2024-03-13 16:57:15 +0600

    BringUp: Superior Extended 2
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 # repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10
 repo init -u https://github.com/SuperiorExtended/manifest -b UDC --git-lfs
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b sup-ex .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b 14.0 .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
