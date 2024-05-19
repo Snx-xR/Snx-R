e5430d568a35a625445292d3e248cbfb2541a57f
Sã Śâjjãd <sqsa93103@gmail.com>
2024-05-19 22:24:10 +0600

    Lets go boy
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # add repo, dt, vt, kt etc.
-repo init -u https://github.com/ResurrectionRemix-Revived/manifest.git -b Q --git-lfs
+repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-17.1 --git-lfs
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b rros .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b los .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
-lunch rr_lavender-userdebug
+lunch lineage_lavender-userdebug
 # export extra module
 export TZ=Asia/Dhaka
 # export SELINUX_IGNORE_NEVERALLOWS=true
@@ -15,5 +15,5 @@ make test-api-stubs-docs || echo no problem
 # vanilla build
 m bacon
 export GAPPS_BUILD=false
-export RELEASE=true
+export RELEASE=false
 Snx-R
