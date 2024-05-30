5e66c9121a590f450d827fbec98e87a0033c16bf
Sã Śâjjãd <sqsa93103@gmail.com>
2024-05-30 15:04:40 +0600

    fix-up
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- twrp.sh
+++ twrp.sh
@@ -10,10 +10,11 @@ git clone https://gitlab.com/OrangeFox/sync.git # (or, using ssh, "git clone git
 cd ~/OrangeFox_sync/sync/
 ./orangefox_sync.sh --branch 12.1 --path ~/fox_12.1
 cd ~/fox_12.1
-git clone --depth 1 git@github.com:Sa-Sajjad/kernel_xiaomi_lavender-udc.git kernel/xiaomi/lavender
+# git clone --depth 1 git@github.com:Sa-Sajjad/kernel_xiaomi_lavender-udc.git kernel/xiaomi/lavender
 git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git device/xiaomi/lavender
-git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
+# git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
 export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_DYNAMIC_PARTITIONS=0
 source build/envsetup.sh
 lunch twrp_lavender-eng && mka recoveryimage
 
