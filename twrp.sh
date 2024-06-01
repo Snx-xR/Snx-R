4da6d7af41df0aba12d78139ce4247060395b16d
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-01 10:00:51 +0600

    lavender: adapt to twrp recovery
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- twrp.sh
+++ twrp.sh
@@ -1,8 +1,3 @@
-sudo apt install git aria2 -y
-git clone https://gitlab.com/OrangeFox/misc/scripts
-cd scripts
-sudo bash setup/android_build_env.sh
-sudo bash setup/install_android_sdk.sh
 mkdir ~/twrp
 cd ~/twrp
 repo init  --depth=1 --no-repo-verify -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1
@@ -11,6 +6,9 @@ repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git -b twrp device/xiaomi/lavender
 # git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
 export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_LAVENDER_KERNEL=4.4
+export FOX_BUILD_FULL_KERNEL_SOURCES=0
+export FOX_USE_DYNAMIC_PARTITIONS=0
 source build/envsetup.sh
 lunch twrp_lavender-eng && mka recoveryimage
 cd out/target/product/$device
