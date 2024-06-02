6a0243ff17d5ec239927d42a0b6076ecb1597da3
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-02 14:45:49 +0600

    fix-up
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -5,9 +5,9 @@ git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
-lunch nad_lavender-userdebug
-# export extra module
 export TZ=Asia/Dhaka
+lunch nad_lavender-user
+# export extra module
 # export SELINUX_IGNORE_NEVERALLOWS=true
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
