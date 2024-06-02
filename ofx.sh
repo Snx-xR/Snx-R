ac9daa04f2d0bd24c175e9f97a55027cc35d30a4
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-02 07:44:54 +0600

    fix-up
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- ofx.sh
+++ ofx.sh
@@ -10,11 +10,12 @@ git clone https://gitlab.com/OrangeFox/sync.git # (or, using ssh, "git clone git
 cd ~/OrangeFox_sync/sync/
 ./orangefox_sync.sh --branch 12.1 --path ~/OrangeFox
 cd ~/OrangeFox
-# git clone --depth 1 git@github.com:Sa-Sajjad/kernel_xiaomi_lavender-udc.git kernel/xiaomi/lavender
-git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git -b 14.0 device/xiaomi/lavender
+git clone --depth 1 git@github.com:Sa-Sajjad/kernel_xiaomi_lavender-udc.git -b 4.4 kernel/xiaomi/lavender
+git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git -b ofx device/xiaomi/lavender
 # git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
 export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_BUILD_FULL_KERNEL_SOURCES=1
 source build/envsetup.sh
 lunch twrp_lavender-eng && mka recoveryimage
-rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.img sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/
-rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.zip sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/
+rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.img sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/recovery/
+rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.zip sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/recovery/
