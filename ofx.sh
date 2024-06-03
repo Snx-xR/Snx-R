c21245d06668e598125b9d5eb68d84b5c2d1eb8d
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-03 09:11:38 +0600

    fix-up
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- ofx.sh
+++ ofx.sh
@@ -15,6 +15,22 @@ git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git -b ofx device
 # git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_BUILD_FULL_KERNEL_SOURCES=1
+export FOX_ENABLE_APP_MANAGER=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.4.zip
+export FOX_ASH_IS_BASH=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_SED_BINARY=1
+export OF_MAINTAINER=Sã Śâjjãd
+export FOX_VERSION="R11.1"
+export FOX_BUILD_TYPE=beta-qpr2
+export FOX_ARCH=arm64
+export FOX_VARIANT="12.1"
+export TARGET_DEVICE_ALT="lavender"
+export FOX_USE_XZ_UTILS=1
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"
 source build/envsetup.sh
 lunch twrp_lavender-eng && mka recoveryimage
 rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.img sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/recovery/
