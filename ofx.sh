87f6703308e338b79a736748105977d9f5826a14
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-04 07:51:13 +0600

    fix-up
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- ofx.sh
+++ ofx.sh
@@ -11,7 +11,7 @@ cd ~/OrangeFox_sync/sync/
 ./orangefox_sync.sh --branch 12.1 --path ~/OrangeFox
 cd ~/OrangeFox
 git clone --depth 1 git@github.com:Sa-Sajjad/kernel_xiaomi_lavender-udc.git -b 4.4 kernel/xiaomi/lavender
-git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git -b fox device/xiaomi/lavender
+git clone --depth 1 git@github.com:Sa-Sajjad/recovery_lavender.git -b ofx device/xiaomi/lavender
 # git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_BUILD_FULL_KERNEL_SOURCES=1
