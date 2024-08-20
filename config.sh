96fb413f763e966798d5347ba7f6f6d963d22167
Sa Sajjad <sqsa93103@gmail.com>
2024-08-20 15:37:32 +0600

    lavender : Drop in-tree hals
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 # add repo, dt, vt, kt etc.
 # repo init -u https://github.com/PixelBuildsROM/android_manifest -b unity --depth=1
-repo init -u https://github.com/AOSPA/manifest -b uvite
+repo init -u https://github.com/AOSPA/manifest -b uvite --depth=1
 # replace with your manifest
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b aospa .repo/local_manifests
 # sync script
@@ -17,3 +17,7 @@ export TZ=Asia/Dhaka
 export GAPPS_BUILD=false
 export RELEASE=false
 Snx-R
+
+# crave run --no-patch "source build/envsetup.sh && lunch aospa_lavender-eng
+# export TZ=Asia/Dhaka
+# ./rom-build.sh lavender"
