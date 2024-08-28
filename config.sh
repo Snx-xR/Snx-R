30b549e37a3acab86615668c5855d0baf1fd33c7
Sa Sajjad <sqsa93103@gmail.com>
2024-08-28 08:57:18 +0600

    lavender: Qassa bringup
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,23 +1,24 @@
 # add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
+# repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
+repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/keepQASSA/manifest -b Q
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b qassa .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
+
 source build/envsetup.sh
-rm -rf vendor/lawnchair
 export TZ=Asia/Dhaka
-export USE_GAPPS=true
-export WITH_GAPPS=true
-export EXTRA_GAPPS=false
-lunch nad_lavender-user
+# export USE_GAPPS=true
+# export WITH_GAPPS=true
+# export EXTRA_GAPPS=false
+lunch qassa_lavender-user
 # export extra module
 # export SELINUX_IGNORE_NEVERALLOWS=true
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-mka nad
+mka qassa -j$(nproc --all)
 export GAPPS_BUILD=false
-export RELEASE=true
+export RELEASE=no
 Snx-R
