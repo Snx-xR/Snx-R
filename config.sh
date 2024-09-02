3f5788a8fec9fc87c9314f14213212937902e437
Sa Sajjad <sqsa93103@gmail.com>
2024-09-02 06:22:13 +0600

    lav fix2
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # add repo, dt, vt, kt etc.
 # repo init -u https://github.com/PixelBuildsROM/android_manifest -b unity --depth=1
-repo init -u https://github.com/AOSPA/manifest -b uvite --depth=1
+repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b aospa .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b orion .repo/local_manifests
 # sync script
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh && lunch aospa_lavender-eng
+source build/envsetup.sh && lunch orion_lavender-ap2a-userdebug
 export TZ=Asia/Dhaka
 # export extra module
 # export SELINUX_IGNORE_NEVERALLOWS=true
@@ -13,7 +13,7 @@ export TZ=Asia/Dhaka
 # make system-api-stubs-docs || echo no problem
 # make test-api-stubs-docs || echo no problem
 # vanilla build
-./rom-build.sh lavender
+mka space
 export GAPPS_BUILD=false
 export RELEASE=false
 Snx-R
