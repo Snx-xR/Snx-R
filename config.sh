ae1a7b63ee2b83dce2844d90ee9d2964123fd106
Sa Sajjad <sqsa93103@gmail.com>
2024-09-15 06:32:28 +0600

    lav qti
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # add repo, dt, vt, kt etc.
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-qti .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
