b2550b079f03ad6f0cab38cfc3ced4f96ddab24b
Sa Sajjad <sqsa93103@gmail.com>
2024-10-04 08:58:35 +0600

    lav fix2
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -17,7 +17,7 @@ lunch nad_lavender-user
 # make test-api-stubs-docs || echo no problem
 # vanilla build
 # mka nad
-m framework-res
+m settings
 export GAPPS_BUILD=false
 export RELEASE=true
 Snx-R
