1460107693602160e32aae8ec89a3cc106750a91
Sa Sajjad <sqsa93103@gmail.com>
2024-10-10 22:31:06 +0600

    nusan tara final
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # add repo, dt, vt, kt etc.
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-qti .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
@@ -16,10 +16,9 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 # make system-api-stubs-docs || echo no problem
 # make test-api-stubs-docs || echo no problem
 # vanilla build
-# mka nad
+mka nad
 # m framework
-m Settings
-export GAPPS_BUILD=false
-export RELEASE=false
+# m Settings
+export RELEASE=true
 # rsync -vhcP -e ssh out/target/product/lavender/system/product/priv-app/Settings/Settings.apk sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/app/
 Snx-R
