6a5d157f1bbe8f3bb6a5db261ed553199a003306
Sã Śâjjãd <mdsojola12@gmail.com>
2024-10-31 23:38:47 +0600

    qasssa
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 # add repo, dt, vt, kt etc.
 # repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/keepQASSA/manifest -b Q
+repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/keepQASSA/manifest -b Q
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b qassa .repo/local_manifests
+git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b qassa .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
@@ -14,9 +14,9 @@ export TZ=Asia/Dhaka
 lunch qassa_lavender-user
 # export extra module
 # export SELINUX_IGNORE_NEVERALLOWS=true
-make api-stubs-docs || echo no problem
-make system-api-stubs-docs || echo no problem
-make test-api-stubs-docs || echo no problem
+# make api-stubs-docs || echo no problem
+# make system-api-stubs-docs || echo no problem
+# make test-api-stubs-docs || echo no problem
 # vanilla build
 mka qassa -j$(nproc --all)
 export GAPPS_BUILD=false
