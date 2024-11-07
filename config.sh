10eea219e96ba8d1e10f3d8ba0db6a791ea6e7cf
Sã Śâjjãd <mdsojola12@gmail.com>
2024-11-07 13:04:16 +0600

    g
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -1,24 +1,17 @@
 # add repo, dt, vt, kt etc.
-# repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
-repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/keepQASSA/manifest -b Q
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 # replace with your manifest
-git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b qassa .repo/local_manifests
+git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b cr-14 .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
-
+# /opt/crave/resync.sh
+# lunch combo paste here
 source build/envsetup.sh
-export TZ=Asia/Dhaka
-# export USE_GAPPS=true
-# export WITH_GAPPS=true
-# export EXTRA_GAPPS=false
-lunch qassa_lavender-user
-# export extra module
-# export SELINUX_IGNORE_NEVERALLOWS=true
-# make api-stubs-docs || echo no problem
-# make system-api-stubs-docs || echo no problem
-# make test-api-stubs-docs || echo no problem
-# vanilla build
-mka qassa -j$(nproc --all)
-export GAPPS_BUILD=false
-export RELEASE=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+breakfast lavender
+# export TZ=Asia/Dhaka
+# make installclean
+# brunch lavender
+cd device/xiaomi/lavender/parts
+mm
 Snx-R
