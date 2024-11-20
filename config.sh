bd05b0913763a2ae75df86af99d5497230486289
Sa Sajjad <mdsojola12@gmail.com>
2024-11-20 22:35:30 +0600

    lavender: nusa stock version

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 # add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
+repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b stock
 # replace with your manifest
 git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b ten-ksu .repo/local_manifests
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 export TZ=Asia/Dhaka
-export USE_GAPPS=true
-export WITH_GAPPS=true
-export EXTRA_GAPPS=false
+# export USE_GAPPS=true
+# export WITH_GAPPS=true
+# export EXTRA_GAPPS=false
 lunch nad_lavender-user
 # export extra module
 export SELINUX_IGNORE_NEVERALLOWS=true
@@ -18,5 +18,5 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 mka nad
 # m framework
 # m Settings
-export RELEASE=true
+export RELEASE=no
 Snx-R
