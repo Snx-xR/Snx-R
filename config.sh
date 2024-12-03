bb16d530da6c7460be745efa33939668c27d7375
Sa Sajjad <mdsojola12@gmail.com>
2024-12-03 10:02:39 +0600

    try nusa
    Signed-off-by: Sa Sajjad <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -15,9 +15,9 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 # make api-stubs-docs || echo no problem
 # make system-api-stubs-docs || echo no problem
 # make test-api-stubs-docs || echo no problem
-# mka nad
-make SystemUI -j$(nproc)
-mmm packages/apps/Launcher3 -j$(nproc)
+mka nad
+# make SystemUI -j$(nproc)
+# mmm packages/apps/Launcher3 -j$(nproc)
 # m framework
 # m Settings
 export RELEASE=no
