7d1b926cdbf5b24e47d2b3446254e30cc09f7612
Sa Sajjad <mdsojola12@gmail.com>
2024-12-05 22:10:36 +0600

    qassa again
    Signed-off-by: Sa Sajjad <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,10 @@
 # add repo, dt, vt, kt etc.
-# repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
-repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/keepQASSA/manifest -b Q
+# repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/keepQASSA/manifest -b Q
+repo init -u ssh://git@github.com/keepQASSA/manifest -b Q
 # replace with your manifest
 git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b qassa .repo/local_manifests
 # sync script
-repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
-
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 export TZ=Asia/Dhaka
 # export USE_GAPPS=true
@@ -19,6 +18,5 @@ lunch qassa_lavender-user
 # make test-api-stubs-docs || echo no problem
 # vanilla build
 mka qassa -j$(nproc --all)
-export GAPPS_BUILD=false
-export RELEASE=true
+export RELEASE=no
 Snx-R
