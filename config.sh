6247b254139578d82be8a6d7c2e170679a330801
Sa Sajjad <mdsojola12@gmail.com>
2024-12-23 21:49:39 +0600

    Nusan final build
    Signed-off-by: Sa Sajjad <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -6,17 +6,16 @@ git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b ten-ksu .repo/loc
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 export TZ=Asia/Dhaka
+
 export USE_GAPPS=true
 export WITH_GAPPS=true
 export EXTRA_GAPPS=false
+
 lunch nad_lavender-user
-# export extra module
 export SELINUX_IGNORE_NEVERALLOWS=true
 # make api-stubs-docs || echo no problem
 # make system-api-stubs-docs || echo no problem
 # make test-api-stubs-docs || echo no problem
 mka nad
-# m framework
-# m Settings
 export RELEASE=true
 Snx-R
