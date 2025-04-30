30d607d014964407405409d8b64f66b191e20a08
Sã Śâjjãd <mdsojola12@gmail.com>
2025-04-30 15:42:21 +0600

    Revert "lavender:"
    This reverts commit 48602fa494fc24447de59888ed2983c79af5567a.

--- ofx.sh
+++ ofx.sh
@@ -4,16 +4,21 @@
 # cd scripts
 # sudo bash setup/android_build_env.sh
 # sudo bash setup/install_android_sdk.sh
-git clone https://gitlab.com/OrangeFox/sync.git; cd sync # (or, using ssh, "git clone git@gitlab.com:OrangeFox/sync.git")
-./orangefox_sync.sh --branch 12.1 --_path /tmp/src/android
-cd /tmp/src/android
+/bin/bash
+mkdir ~/OrangeFox_sync
+cd ~/OrangeFox_sync
+git clone https://gitlab.com/OrangeFox/sync.git # (or, using ssh, "git clone git@gitlab.com:OrangeFox/sync.git")
+cd ~/OrangeFox_sync/sync/
+./orangefox_sync.sh --branch 12.1 --path ~/OrangeFox
+cd ~/OrangeFox
+# git clone --depth 1 https://github.com/Sa-Sajjad/kernel_xiaomi_lavender-4.19 kernel/xiaomi/lavender
 git clone --depth 1 https://github.com/Sa-Sajjad/device_lavender_recovery device/xiaomi/lavender
+# git clone --depth 1 -b nexus-14  https://gitlab.com/Project-Nexus/nexus-clang.git clang
 
+
+export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_BUILD_DEVICE=lavender
 export LC_ALL="C"
-
-source build/envsetup.sh
-lunch twrp_lavender-eng && mka adbd recoveryimage
 
 # export FOX_BUILD_FULL_KERNEL_SOURCES=1
 # export FOX_ENABLE_APP_MANAGER=1
@@ -33,6 +38,7 @@ lunch twrp_lavender-eng && mka adbd recoveryimage
 # export FOX_USE_XZ_UTILS=1
 # export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"
 
-
+source build/envsetup.sh
+lunch twrp_lavender-eng && mka adbd recoveryimage
 rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.img sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/recovery/
 rsync -vhcP -e ssh out/target/product/$device/OrangeFox*.zip sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/$device/recovery/
