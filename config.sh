ada8cb9430a3dc4b992d1c79b64770249fe1b60f
Sã Śâjjãd <mdsojola12@gmail.com>
2025-05-20 08:31:52 +0600

    here we again
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 # add repo, dt, vt, kt etc.
 # repo init --depth=1 --no-repo-verify -u https://Snax-phycho:$ght@github.com/keepQASSA/manifest -b Q
-repo init -u ssh://git@github.com/keepQASSA/manifest -b Q
+repo init -u ssh://git@github.com/keepQASSA/manifest -b Q --git-lfs
 # replace with your manifest
 git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b qassa .repo/local_manifests
 # sync script
