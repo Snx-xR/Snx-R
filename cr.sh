779afb8e85a3dc2e88860526e341d0065273fc34
Sã Śâjjãd <mdsojola12@gmail.com>
2025-05-22 17:25:57 +0600

    lavender: Drop ext4 support for userdata
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- cr.sh
+++ cr.sh
@@ -2,3 +2,4 @@ source build/envsetup.sh
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
 brunch lavender
+bash <(curl -s https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/refs/heads/master/upload.sh) out/target/product/*/*.zip || curl -O https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/refs/heads/master/upload.sh && upload.sh out/target/product/*/*.zip
