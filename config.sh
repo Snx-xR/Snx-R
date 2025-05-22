57a2909012a79a65ae2180f38df733aa6fb951cd
Sã Śâjjãd <mdsojola12@gmail.com>
2025-05-22 08:26:47 +0600

    lavender: switch to nexus kernel
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- config.sh
+++ config.sh
@@ -3,10 +3,11 @@ repo init -u https://github.com/crDroid-Revived/android.git -b 11.0 --git-lfs
 # replace with your manifest
 git clone git@github.com:Snax-phycho/manifest.git --depth 1 -b cr .repo/local_manifests
 # sync script
-repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --prune
 
 source build/envsetup.sh
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
+lunch lineage_lavender-userdebug || echo "no problem"
 brunch lavender
 Snx-R
