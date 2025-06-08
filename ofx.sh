3755b65b14a52b10c1ee2a2e275f96e41d96b665
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2025-06-08 12:10:49 +0600

    ofx
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- ofx.sh
+++ ofx.sh
@@ -13,6 +13,6 @@ git clone --depth 1 git@github.com:Sa-Sajjad/device_lavender_recovery.git -b 15 
 # export LC_ALL="C"
 
 source build/envsetup.sh
-# source $CIRRUS_WORKING_DIR/env.sh
+export FOX_BUILD_DEVICE=lavender
 lunch twrp_lavender-eng && mka adbd recoveryimage
 bash $CIRRUS_WORKING_DIR/gh.sh
