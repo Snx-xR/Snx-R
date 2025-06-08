34cf34fa354fff2b8d175fd52e6e4faefd8a519d
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2025-06-08 09:15:03 +0600

    ofx
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- ofx.sh
+++ ofx.sh
@@ -8,9 +8,9 @@ cd ~/OrangeFox
 # git clone --depth 1 https://github.com/Sa-Sajjad/kernel_xiaomi_lavender-4.19 kernel/xiaomi/lavender
 git clone --depth 1 git@github.com:Sa-Sajjad/device_lavender_recovery.git -b 15 device/xiaomi/lavender
 
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_BUILD_DEVICE=lavender
-export LC_ALL="C"
+# export ALLOW_MISSING_DEPENDENCIES=true
+# export FOX_BUILD_DEVICE=lavender
+# export LC_ALL="C"
 
 source build/envsetup.sh
 lunch twrp_lavender-eng && mka adbd recoveryimage
