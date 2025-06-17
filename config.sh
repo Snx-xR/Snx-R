58cda6defa49e61b15fa3dcef0444402f5bcda41
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2025-06-17 09:27:54 +0600

    fy

--- config.sh
+++ config.sh
@@ -1,32 +1,55 @@
-# add repo, dt, vt, kt etc.
-repo init --depth=1 --git-lfs --no-repo-verify https://github.com/ProjectMatrixx/android.git -b 14.0 -g default,-mips,-darwin,-notdefault
-# replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b mtx-14 .repo/local_manifests
-# sync script
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#*********************************************************************************************************#
+send_message() {
+    local RESPONSE=$(curl "https://api.telegram.org/bot$BOT_API/sendMessage" -d chat_id="$TELEGRAM_ID" \
+        -d "parse_mode=html" \
+        -d "disable_web_page_preview=true" \
+        -d text="$1")
+    local MESSAGE_ID=$(echo "$RESPONSE" | grep -o '"message_id":[0-9]*' | cut -d':' -f2)
+    echo "$MESSAGE_ID"
+}
+#*********************************************************************************************************#
+send_file_to_error_chat() {
+    curl --progress-bar -F document=@"$1" "https://api.telegram.org/bot$BOT_API/sendDocument" \
+        -F chat_id="$TELEGRAM_ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" >& /dev/null
+}
+#*********************************************************************************************************#
+upload_to_gofile() {
+    SERVER=$(curl -s https://api.gofile.io/servers | jq -r '.data.servers[0].name')
+    LINK=$(curl -# -F "file=@$1" "https://${SERVER}.gofile.io/uploadFile" | jq -r '.data|.downloadPage') 2>&1
+    echo "$LINK"
+    export  GF_LINK="$LINK"
+    send_message "$GF_LINK"
+}
+#*********************************************************************************************************#
+
+send_message "Build started on cirrus"
+
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+# rm -rf packages/apps/LineageParts vendor/extra
+# curl -s $SSH | bash > /dev/null 2>&1; # sudo apt update && sudo apt install ccache -y # > /dev/null 2>&1
+rm -rf .repo/local_manifests; git clone git@github.com:Sa-Sajjad/manifest.git -b cr-15 .repo/local_manifests
+# /opt/crave/resync.sh;
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+rm -rf hardware/qcom-caf/sdm660/display; git clone https://github.com/Sa-Sajjad/android_hardware_qcom_display -b vic hardware/qcom-caf/sdm660/display
+# rm -rf build/soong; git clone https://github.com/zeydann/android_build_soong-axion.git -b lineage-22.2 build/soong
+
+# rm -rf -v hardware/qcom-caf/sdm660/au* hardware/qcom-caf/sdm660/me* hardware/qcom-caf/sdm660/di*
+# git clone https://github.com/pix106/android_hardware_qcom-caf_display hardware/qcom-caf/sdm660/display
+# git clone https://github.com/pix106/android_hardware_qcom-caf_audio hardware/qcom-caf/sdm660/audio
+# git clone https://github.com/pix106/android_hardware_qcom-caf_media hardware/qcom-caf/sdm660/media
 
 source build/envsetup.sh
-lunch lineage_lavender-userdebug
-
-# export extra module
 export TZ=Asia/Dhaka
-export SELINUX_IGNORE_NEVERALLOWS=true
-export RELAX_USES_LIBRARY_CHECK=true
-export SKIP_ABI_CHECKS=true
-export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
-export RELAX_USES_LIBRARY_CHECK=true
-# make api-stubs-docs || echo no problem
-# make system-api-stubs-docs || echo no problem
-# make test-api-stubs-docs || echo no problem
-# vanilla build
-m bacon
+breakfast lavender
+make installclean
+brunch lavender
 
-# gapps build
-# export USE_GAPPS=true
-# mka nad
+echo -e "uploading on gofile"
+# upload_to_gofile out/target/product/*/*.zip # || send_file_to_error_chat out/error.log && exit 1
 
-# # only specific app build
-# export BOARD_USES_SYSTEMUI=true
-# mka SystemUI
-# bash <(curl -s $script/upload_apps.sh)
-Snx-R
+[ -f out/target/product/*/*.zip ] && upload_to_gofile out/target/product/*/*.zip || { send_file_to_error_chat out/error.log; exit 1; }
+
+echo -e "Enjoy 🎉"
