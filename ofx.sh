f44157f4b3b83e284cb8b34757f5231045fa75f2
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2025-06-30 18:45:54 +0600

    fox
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- ofx.sh
+++ ofx.sh
@@ -9,6 +9,7 @@ cd ~/OrangeFox
 git clone --depth 1 git@github.com:Sa-Sajjad/device_lavender_recovery.git -b 15 device/xiaomi/lavender
 
 export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/data/recovery/"
+export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=0
 source build/envsetup.sh
 lunch twrp_lavender-eng && mka adbd recoveryimage
 bash $CIRRUS_WORKING_DIR/gh.sh
